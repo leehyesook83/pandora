09e000b211a7e9a850eb10fbb31fcaa039a100dd
leehyesook83 <aagiya@naver.com>
2024-08-09 17:05:28 +0900

    fix

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ import ScrollToTop from './ScrollToTop'
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
     <ScrollToTop>
     <App />
     </ScrollToTop>
